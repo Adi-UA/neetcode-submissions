bd59b71636371729a5774e21b777142fec50e02b
Adi <abanerjee.email@gmail.com>
2026-09-09 13:23:38 -0700

    Add: design-word-search-data-structure - submission-10

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-10.py
@@ -0,0 +1,34 @@
+class TrieNode:
+    def __init__(self,children=None,eow=False):
+        self.children={} if not children else children
+        self.eow=eow
+class WordDictionary:
+
+    def __init__(self):
+        self.root=TrieNode()
+
+    def addWord(self, word: str) -> None:
+        node=self.root
+        for char in word:
+            # check if char matches any chars under node
+            node.children[char]=node.children.get(char,TrieNode())
+            node = node.children[char]
+        # add eow marker to last node
+        node.eow=True
+
+    def search(self, word: str) -> bool:
+        def dfs(node,j):
+            # check down the word
+            for i in range(j,len(word)):
+                c=word[i]
+                if c == ".":
+                    for child in node.children.values(): # child nodes
+                        if dfs(child,i+1):
+                            return True
+                    return False # didn't find match
+                elif c in node.children:
+                    node=node.children[c]
+                else:
+                    return False
+            return node.eow
+        return dfs(self.root,0)  
