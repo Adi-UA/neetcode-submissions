6b296bf06b37bee90f40b7cfc0bd119de048897b
Adi <abanerjee.email@gmail.com>
2026-09-10 20:01:59 -0700

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        def maxrob(l,u):
+            lrob,rrob=0,0
+            for n in nums[l:u]:
+                tmp=lrob
+                lrob=rrob
+                rrob=max(n + tmp,lrob)
+            return rrob
+        return max(maxrob(0,len(nums)-1),maxrob(1,len(nums)))
